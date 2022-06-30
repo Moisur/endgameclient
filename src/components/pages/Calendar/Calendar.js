05229086c961032903fe3d0fc4c1ad8526a3d891
Mosiur islam <Mosiurislamwebdesing@gmail.com>
2022-07-01 02:53:41 +0600

    Time Add

--- src/components/pages/Calendar/Calendar.js
+++ src/components/pages/Calendar/Calendar.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
 
+import "react-datepicker/dist/react-datepicker.css";
 const Calendar = () => {
+    const [startDate, setStartDate] = useState(new Date());
     return (
         <div>
-            This is Calendar 
+            <div>
+                <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+            </div>
         </div>
     );
 };
